511813fbeb223fb510dd90ec6e56eb1d6f9f8146
nicosanfurgo <nicosanmza@hotmail.com>
2023-02-06 21:34:31 -0300

    arreglos

--- app.js
+++ app.js
@@ -1,27 +1,42 @@
+const ProductManager = require("./ProductManager");
 
 const express = require("express");
 const path = require("path")
 
 const app = express();
 
-app.listen(3000, ()=>{
-    console.log("Servidor levantado", 3000);
-});
 
+app.listen(8080, ()=>{
+    console.log("Servidor levantado", 8080);
+});
 
 app.get("/", (req, res)=>{
     res.send("Desafio clase 7")
 });
 
-app.get("/products", (req, res) => {
-    res.sendFile(path.join(__dirname + "/products.json"))
+
+const manager = new ProductManager("products.json");
+
+app.get("/products", async (req, res) => {
+    let products = await manager.getProducts();
+    const limit = req.query.limit;
+    let result = products
+
+    if (limit) {
+        result = products.slice(0, limit);
+    }
+
+    res.send(result);
 })
 
-app.get('/products/:id', (req, res) => {
-    const {id} = res.params
-    const product = product.find(item => item.id === id)
-    res.send({product: product.title})
-    if (!product) res.send({ error: "User not found" })
-    else res.send({ product: product.title })
+app.get('/products/:id', async (req, res) => {
+    let productId = req.params.id;
+    let product = await manager.getProductById(parseInt(productId));
+
+    if (!product) {
+        return res.send({ error: "Producto invalido o no encontrado"});
+    } else {
+        res.send({product});
+    }
 })
 
